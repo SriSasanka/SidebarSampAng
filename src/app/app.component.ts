a2077adcd8f23b1ec3fdfb2937a34252316f6574
SriSasanka <srisasanka.is@gmail.com>
2018-07-01 01:38:00 +0530

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from '@angular/core';
+
+import  * as $ from 'jquery';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'app';
+  ngOnInit() {
+  $('#sidebar-toggler').click(function (e) {
+      e.preventDefault();
+      $("#targetCollapse").toggleClass('active');
+    })
+
+  }
+}
